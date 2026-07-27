920b8e1942baba929cdb45ed9c94761b45a4da24
phattar4phan <phattar4phan@gmail.com>
2026-07-27 21:35:48 +0700

    add token bin for general.txt

--- src/utils/bins.py
+++ src/utils/bins.py
@@ -1,33 +1,28 @@
 import numpy as np
 import sentencepiece as spm
-from datasets import load_dataset
 
-sp = spm.SentencePieceProcessor()
-sp.load("./token/lanna.model")
+sp = spm.SentencePieceProcessor(model_file="./token/llm.model")
 
-ds = load_dataset("allenai/Dolci-Instruct-SFT")
-
-print("Encoding...")
-
-with open("./data/train.bin", "wb") as f:
-    for i, row in enumerate(ds["train"]):
-        text = ""
-
-        for msg in row["messages"]:
-            content = msg.get("content")
-            if content is None:
+def tokenizeIDs(input_path: str, output_path: str):
+    with open(input_path, 'r', encoding='utf-8') as fin, open(output_path, 'wb') as fout:
+        total_tokens = 0
+        
+        for i, line in enumerate(fin):
+            line = line.strip()
+            
+            if not line:
                 continue
-
-            text += f"<|{msg['role']}|>\n"
-            text += content.strip()
-            text += "\n\n"
-
-        text += "<|end|>\n"
-
-        ids = sp.encode(text, out_type=int)
-        np.array(ids, dtype=np.uint32).tofile(f)
-
-        if i % 10000 == 0:
-            print(f"processed {i} samples")
-
-print("Done.")
+            
+            ids = sp.encode(line + "\n<|end|>\n", out_type=int)
+            
+            np.array(ids, dtype=np.uint32).tofile(fout)
+            
+            total_tokens += len(ids)
+            
+            if i % 10000 == 0:
+                print(f'\rProcessed: {i:,} lines | {total_tokens:,} tokens', end='', flush=True)
+                
+    print(f'\nDone! total: {total_tokens:,} tokens')
+    
+tokenizeIDs('./data/general.txt', './data/general.bin')
+tokenizeIDs('./data/instruct.txt', './data/instruct.bin')
