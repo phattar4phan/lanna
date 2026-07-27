2d51802838ffc067f5476e61e92deb323e752d34
phattar4phan <phattar4phan@gmail.com>
2026-07-27 21:35:24 +0700

    adjust training pipeline

--- src/train.py
+++ src/train.py
@@ -186,162 +186,130 @@ print(f'{Fore.LIGHTBLUE_EX}Scheduler{Fore.RESET}: {scheduler}')
 
 scaler = torch.amp.GradScaler("cuda", enabled=DEVICE == "cuda")
 
-val_loader = create_loader(VAL_PATH)
+def train(paths, stage_name, epochs, val_path):
+    for epoch in range(epochs):
+        model.train()
 
-for epoch in range(EPOCHS):
-    model.train()
+        total_loss = 0
+        total_batches = 0
+        total_correct = 0
+        total_tokens = 0
 
-    total_loss = 0
-    total_correct = 0
-    total_tokens = 0
+        start = time.perf_counter()
 
-    start = time.time()
+        for path in paths:
+            print(f"\n{stage_name}: {path}")
 
-    for shard in SHARDS:
+            loader = create_loader(path)
+
+            bar = tqdm(
+                loader,
+                desc=f"{stage_name} Epoch {epoch+1}/{epochs}",
+                unit="batch",
+                dynamic_ncols=True
+            )
+
+            for step, (x, y) in enumerate(bar):
+                x = x.to(DEVICE, non_blocking=True)
+                y = y.to(DEVICE, non_blocking=True)
+
+                with torch.autocast(device_type="cuda", dtype=DTYPE, enabled=DEVICE=="cuda"):
+                    logits, loss = model(x, y)
+                    loss_scaled = loss / GRAD_ACCUM
+
+                scaler.scale(loss_scaled).backward()
+
+                if (step + 1) % GRAD_ACCUM == 0:
+                    scaler.unscale_(optimizer)
+
+                    torch.nn.utils.clip_grad_norm_(
+                        model.parameters(),
+                        MAX_GRAD_NORM
+                    )
+
+                    scaler.step(optimizer)
+                    scaler.update()
+
+                    optimizer.zero_grad(set_to_none=True)
+
+                total_loss += loss.item()
+                total_batches += 1
+
+                c, t = accuracy(logits,y)
+
+                total_correct += c
+                total_tokens += t
+
+                elapsed = time.perf_counter() - start
+                speed = (step + 1) / elapsed
+                remaining = (len(loader) - step - 1) / speed
+
+                acc = total_correct / total_tokens * 100
+
+                bar.set_postfix_str(
+                    f"{PERCENT}{bar.n / bar.total * 100:.2f}% "
+                    f"{BATCH}{speed:.1f} batch/s "
+                    f"{TIME_GREEN}{format_time(elapsed)}"
+                    f"{TIME_CYAN}/{format_time(remaining)} "
+                    f"{LOSS}loss {VAL}{loss.item():.4f} "
+                    f"{ACC}acc {VAL}{acc:.2f}%"
+                )
+
+            print(
+                f"{stage_name} shard finished"
+            )
+
+        if (step + 1) % GRAD_ACCUM != 0:
+            scaler.unscale_(optimizer)
+
+            torch.nn.utils.clip_grad_norm_(
+                model.parameters(),
+                MAX_GRAD_NORM
+            )
+
+            scaler.step(optimizer)
+            scaler.update()
+
+            optimizer.zero_grad(
+                set_to_none=True
+            )
+
+        train_loss = total_loss / total_batches
+        train_acc = total_correct / total_tokens * 100
+
+        val_loader = create_loader(val_path)
+
+        val_loss, val_acc = evaluate(val_loader)
+
+        scheduler.step(val_loss)
+
         print(
-            f"\nTraining shard: {shard}"
+            f"\n"
+            f"{LOSS}loss {VAL}{train_loss:.4f} "
+            f"{ACC}acc {VAL}{train_acc:.2f}% "
+            f"{VAL}val_loss {val_loss:.4f} "
+            f"{VAL}val_acc {val_acc:.2f}%\n"
         )
 
-        loader = create_loader(shard)
+    torch.save(model.state_dict(),CHECKPOINT_DIR / f"{stage_name}.pt")
 
-        bar = tqdm(
-            loader,
-            desc=f"Epoch {epoch+1}/{EPOCHS}",
-            unit="batch",
-            dynamic_ncols=True
-        )
+    print(f"Saved {stage_name} checkpoint")
 
-        for step, (x, y) in enumerate(bar):
-            x = x.to(
-                DEVICE,
-                non_blocking=True
-            )
-
-            y = y.to(
-                DEVICE,
-                non_blocking=True
-            )
-
-            with torch.autocast(device_type="cuda", dtype=DTYPE, enabled=DEVICE=="cuda"):
-                logits, loss = model(x, y)
-
-                loss_scaled = (
-                    loss /
-                    GRAD_ACCUM
-                )
-
-            scaler.scale(
-                loss_scaled
-            ).backward()
-
-            if (
-                step + 1
-            ) % GRAD_ACCUM == 0:
-
-                scaler.unscale_(
-                    optimizer
-                )
-
-                torch.nn.utils.clip_grad_norm_(
-                    model.parameters(),
-                    MAX_GRAD_NORM
-                )
-
-                scaler.step(
-                    optimizer
-                )
-
-                scaler.update()
-
-                optimizer.zero_grad(
-                    set_to_none=True
-                )
-
-            total_loss += loss.item()
-
-            c, t = accuracy(
-                logits,
-                y
-            )
-
-            total_correct += c
-            total_tokens += t
-
-            elapsed = (
-                time.time()
-                -
-                start
-            )
-
-            speed = (
-                step + 1
-            ) / elapsed
-
-            remaining = (
-                len(loader)
-                -
-                step
-                -
-                1
-            ) / speed
-
-            acc = (
-                total_correct /
-                total_tokens *
-                100
-            )
-
-            bar.set_postfix_str(
-
-                f"{PERCENT}"
-                f"{(bar.n / bar.total * 100):.2f}% "
-                f"{BATCH}"
-                f"{speed:.1f} batch/s "
-                f"{TIME_GREEN}"
-                f"{format_time(elapsed)}"
-                f"{TIME_CYAN}/"
-                f"{format_time(remaining)} "
-                f"{LOSS}"
-                f"loss "
-                f"{VAL}"
-                f"{loss.item():.4f} "
-                f"{ACC}"
-                f"accuracy "
-                f"{VAL}"
-                f"{acc:.2f}%"
-            )
-
-    train_loss = (
-        total_loss /
-        len(SHARDS)
-    )
-
-    train_acc = (
-        total_correct /
-        total_tokens *
-        100
-    )
-
-    val_loss, val_acc = evaluate(
-        val_loader
-    )
-
-    scheduler.step(
-        val_loss
-    )
-
-    print(
-        f"\n"
-        f"{LOSS}loss {VAL}{train_loss:.4f} "
-        f"{ACC}acc {VAL}{train_acc:.2f}% "
-        f"{VAL}val_loss {val_loss:.4f} "
-        f"{VAL}val_acc {val_acc:.2f}%\n"
-    )
-
-torch.save(
-    model.state_dict(),
-    CHECKPOINT_DIR / "lanna.pt"
+train(
+    [GENERAL_PATH],
+    "BASE",
+    3,
+    GENERAL_VAL_PATH
 )
+
+train(
+    SHARDS,
+    "SFT",
+    EPOCHS,
+    INSTRUCT_VAL_PATH
+)
+
+torch.save(model.state_dict(), CHECKPOINT_DIR / "llm.pt")
 
 print("Saved checkpoint")
 
@@ -350,32 +318,27 @@ model.eval()
 dummy = torch.randint(
     0,
     VOCAB_SIZE,
-    (
-        1,
-        CONTEXT_LENGTH
-    ),
+    (1, CONTEXT_LENGTH),
     device=DEVICE
 )
 
 torch.onnx.export(
     model,
     dummy,
-    SRC_DIR / "lanna.onnx",
-    input_names=[
+    SRC_DIR / "llm.onnx",
+    input_names= [
         "input_ids"
     ],
-    output_names=[
+    output_names= [
         "logits"
     ],
-    dynamic_axes={
-        "input_ids":
-        {
+    dynamic_axes= {
+        "input_ids": {
             0:"batch",
             1:"sequence"
         },
 
-        "logits":
-        {
+        "logits": {
             0:"batch",
             1:"sequence"
         }
